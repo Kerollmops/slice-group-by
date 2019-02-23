809bdd49c9b8483f75bf2bbc80b7d205035eb760
Clément Renault <renault.cle@gmail.com>
2019-02-23 11:13:54 +0100

    feat: Introduce the LinearStrGroupBy Iterator

--- src/lib.rs
+++ src/lib.rs
@@ -440,3 +440,108 @@ impl<T> GroupByMut<T> for [T]
         ExponentialGroupMut::new(self)
     }
 }
+
+pub struct LinearStrGroupBy<'a, P> {
+    inner: &'a str,
+    predicate: P,
+}
+
+impl<'a, P> LinearStrGroupBy<'a, P> {
+    pub fn new(string: &'a str, predicate: P) -> Self {
+        Self {
+            inner: string,
+            predicate: predicate,
+        }
+    }
+}
+
+impl<'a, P> Iterator for LinearStrGroupBy<'a, P>
+where P: FnMut(char, char) -> bool,
+{
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.inner.is_empty() { return None }
+
+        let mut iter = self.inner.char_indices().peekable();
+        while let (Some((_, ac)), Some((bi, bc))) = (iter.next(), iter.peek().cloned())
+        {
+            if !(self.predicate)(ac, bc) {
+                let (left, right) = self.inner.split_at(bi);
+                self.inner = right;
+                return Some(left);
+            }
+        }
+
+        let output = self.inner;
+        self.inner = "";
+        return Some(output);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn str_easy() {
+        let string = "aaaabbbbbaacccc";
+
+        let mut iter = LinearStrGroupBy::new(string, |a, b| a == b);
+
+        assert_eq!(iter.next(), Some("aaaa"));
+        assert_eq!(iter.next(), Some("bbbbb"));
+        assert_eq!(iter.next(), Some("aa"));
+        assert_eq!(iter.next(), Some("cccc"));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn str_kanji() {
+        let string = "包包饰饰与与钥钥匙匙扣扣";
+
+        let mut iter = LinearStrGroupBy::new(string, |a, b| a == b);
+
+        assert_eq!(iter.next(), Some("包包"));
+        assert_eq!(iter.next(), Some("饰饰"));
+        assert_eq!(iter.next(), Some("与与"));
+        assert_eq!(iter.next(), Some("钥钥"));
+        assert_eq!(iter.next(), Some("匙匙"));
+        assert_eq!(iter.next(), Some("扣扣"));
+        assert_eq!(iter.next(), None);
+    }
+
+    fn is_cjk(c: char) -> bool {
+        (c >= '\u{2e80}' && c <= '\u{2eff}') ||
+        (c >= '\u{2f00}' && c <= '\u{2fdf}') ||
+        (c >= '\u{3040}' && c <= '\u{309f}') ||
+        (c >= '\u{30a0}' && c <= '\u{30ff}') ||
+        (c >= '\u{3100}' && c <= '\u{312f}') ||
+        (c >= '\u{3200}' && c <= '\u{32ff}') ||
+        (c >= '\u{3400}' && c <= '\u{4dbf}') ||
+        (c >= '\u{4e00}' && c <= '\u{9fff}') ||
+        (c >= '\u{f900}' && c <= '\u{faff}')
+    }
+
+    #[test]
+    fn str_ascii_cjk() {
+        let string = "abc包包bbccdd饰饰ac与与cbca钥钥efgh匙匙brbtb扣扣";
+
+        let mut iter = LinearStrGroupBy::new(string, |a, b| is_cjk(a) == is_cjk(b));
+
+        assert_eq!(iter.next(), Some("abc"));
+        assert_eq!(iter.next(), Some("包包"));
+        assert_eq!(iter.next(), Some("bbccdd"));
+        assert_eq!(iter.next(), Some("饰饰"));
+        assert_eq!(iter.next(), Some("ac"));
+        assert_eq!(iter.next(), Some("与与"));
+        assert_eq!(iter.next(), Some("cbca"));
+        assert_eq!(iter.next(), Some("钥钥"));
+        assert_eq!(iter.next(), Some("efgh"));
+        assert_eq!(iter.next(), Some("匙匙"));
+        assert_eq!(iter.next(), Some("brbtb"));
+        assert_eq!(iter.next(), Some("扣扣"));
+        assert_eq!(iter.next(), None);
+    }
+}
